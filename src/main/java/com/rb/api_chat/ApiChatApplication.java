67e4b8bdf8cb50717d703bbd559007923a41f7b8
rob <254313230+robertomosquera-dev@users.noreply.github.com>
2026-06-23 13:05:50 -0500

    initial commit

---
+++ src/main/java/com/rb/api_chat/ApiChatApplication.java
@@ -0,0 +1,13 @@
+package com.rb.api_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rb/api_chat/ApiChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rb.api_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
